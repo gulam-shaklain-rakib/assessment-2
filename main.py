64afab70faddeef4697239080e8f1bef795f90e6
gulam-shaklain-rakib <30299263@students.lincoln.ac.uk>
2026-04-29 03:20:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -85,14 +85,13 @@ class UserInterface:
         print("Enter appointment date and time (any string is accepted):")
         time = input()
 
-        #--------
-        # modify this line of code to try out your decorator pattern.
+              #--------
+        # Create a new appointment for the selected pet
         appointment = Appointment(pet, time)
         #---------
-
         # add the appointment to the vet's list
-        id = self.vp.create_appointment(appointment)
-        print(f"The appointment ID is {id}")
+appointment_id = self.vp.create_appointment(appointment)
+print(f"The appointment ID is {appointment_id}")
  
     
     def attend_appointment(self):
@@ -119,7 +118,7 @@ class UserInterface:
             return       
         
         # enter owner's and pet's name to find a pet
-        p = self._enter_details_to_find_existing_pet()
+        pet = self._enter_details_to_find_existing_pet()
 
         m = None
         while not m:
@@ -130,8 +129,8 @@ class UserInterface:
         print("Enter dosage (amount to be given):")
         d = int(input())
         
-        id = self.vp.create_prescription(p, m, d)
-        print(f"The prescription ID is {id}")   
+        prescription_id = self.vp.create_prescription(pet, m, d)
+print(f"The prescription ID is {prescription_id}")  
 
     
     def prepare_prescription(self):
