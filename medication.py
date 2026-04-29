f47193c5aac9a22f203e415d8335a8a3e29c1186
gulam-shaklain-rakib <30299263@students.lincoln.ac.uk>
2026-04-29 12:01:37 +0100

    Update medication.py

--- medication.py
+++ medication.py
@@ -1,36 +1,44 @@
-""" medication.py
-contains the medication class 
+"""
+medication.py
+contains the medication class
 """
 
-
-class Medication():
+class Medication:
     def __init__(self, name, amount_in_stock):
         """
-        Medication __init__
-        
-        :param self
-        :param name (string): the name of the medication
-        :param amount_in_stock: how much of this medication is in stock
+        :param name: the name of the medication
+        :param amount_in_stock: how much is in stock
         """
         self.name = name
-        self.amountInStock = amount_in_stock
+        self.amount_in_stock = amount_in_stock
+        self.observers = []  # added for observer pattern
+
+    def attach(self, observer):
+        self.observers.append(observer)
+
+    def detach(self, observer):
+        self.observers.remove(observer)
+
+    def notify_observers(self):
+        for observer in self.observers:
+            observer.update(self)
 
     def restock(self, amount):
-        """        
-        :param self
-        :param amount (int): The amount to increase the stock by
         """
-        self.amountInStock += amount
+        Increase stock by given amount
+        """
+        self.amount_in_stock += amount
+        self.notify_observers()
 
-    
     def reduce_stock(self, amount):
-        self.amountInStock -= amount
-
-
-    def has_enough_stock(self, dosage):        
-        """ Checks if there is enough stock for the given dosage.   
-        :param self
-        :param dosage (int): The dosage to be checked.
-        :returns True or False
         """
-        return self.amountInStock >= dosage
+        Decrease stock by given amount
+        """
+        self.amount_in_stock -= amount
+        self.notify_observers()
+
+    def has_enough_stock(self, dosage):
+        """
+        Check if there is enough stock
+        """
+        return self.amount_in_stock >= dosage
